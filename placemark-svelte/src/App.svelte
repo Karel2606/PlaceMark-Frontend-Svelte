5070184ad3f2798fdd48c7abfd78b51a9b0c0af8
Karel <karel.fischbein@st.oth-regensburg.de>
2022-07-02 00:01:08 +0200

    init Svelte project with vite, added spa routes

---
+++ placemark-svelte/src/App.svelte
@@ -0,0 +1,24 @@
+<script>
+  import Main from "./pages/Main.svelte"
+  import Login from "./pages/Login.svelte"
+  import Signup from "./pages/Signup.svelte"
+  import Router from "svelte-spa-router";
+
+  let routes = {
+    "/": Main,
+    "/login": Login,
+    "/signup": Signup,
+    "/main": Main
+  }
+</script>
+
+<div class="container">
+  <div class="tabs notification is-primary">
+    <ul>
+      <li><a href="/#/">Main</a></li>
+      <li><a href="/#/login">Login</a></li>
+      <li><a href="/#/signup">Signup</a></li>
+    </ul>
+  </div>
+  <Router {routes}/>
+</div>
